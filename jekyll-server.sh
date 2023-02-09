5f1c65b0b9ebe902c427c69a3faaa856066247c4
Paweł 'felixd' Wojciechowski <felixd@konopnickiej.com>
2023-02-09 16:45:39 +0100

    env

--- jekyll-server.sh
+++ jekyll-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-JEKYLL_ENV=production bundle exec jekyll serve --trace
+# JEKYLL_ENV=production bundle exec jekyll serve --trace
+bundle exec jekyll serve -c _config.yml,_environmet.yml --watch --future --livereload
