935afefd78129ef48de5303250044bc873be0a64
Paweł Wojciechowski <pawel@konopnickiej.com>
2023-02-18 15:42:42 +0100

    Contact Form

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -10,7 +10,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "{{ site.formspree_form_url }}/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
+	  var url = "{{ site.formspree_form_url }}";
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
